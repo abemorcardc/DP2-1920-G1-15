9b0090b04a26e0ce83fa7085731c30ce40379498
Flor CL <flocorlop@alum.us.es>
2020-03-25 12:37:32 +0100

    primeros cambios

--- src/main/java/org/springframework/samples/petclinic/web/CitaController.java
+++ src/main/java/org/springframework/samples/petclinic/web/CitaController.java
@@ -16,9 +16,26 @@
 
 package org.springframework.samples.petclinic.web;
 
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Averia;
+import org.springframework.samples.petclinic.model.Cita;
 import org.springframework.samples.petclinic.service.CitaService;
+import org.springframework.samples.petclinic.service.MecanicoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Juergen Hoeller
@@ -29,11 +46,74 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class CitaController {
 
-	private final CitaService citaService;
+	private final CitaService		citaService;
+	private final MecanicoService	mecanicoService;
+	private static final String		VIEWS_MEC_UPDATE_FORM	= "mecanicos/citaMecUpdate";
+
 
 	@Autowired
-	public CitaController(final CitaService clinicService) {
-		this.citaService = clinicService;
+	public CitaController(final MecanicoService mecanicoService, final CitaService citaService) {
+		this.mecanicoService = mecanicoService;
+		this.citaService = citaService;
+	}
+
+	@GetMapping("/mecanicos/citas")
+	public String showMecCitaList(final Principal principal, final Map<String, Object> model) {
+		Integer mecanicoId = this.mecanicoService.findIdByUsername(principal.getName());
+		Collection<Cita> results = this.mecanicoService.findCitasByMecanicoId(mecanicoId);
+		model.put("results", results);
+		return "mecanicos/citaDeMecanicoList";
+	}
+
+	@GetMapping("/mecanicos/citas/{citaId}")
+	public ModelAndView showMecCitaDetalle(@PathVariable("citaId") final int citaId) {
+		ModelAndView mav = new ModelAndView("mecanicos/citaEnDetalle");
+		mav.addObject(this.mecanicoService.findCitaById(citaId));
+		return mav;
+	}
+
+	@GetMapping(value = "/mecanicos/citas/{citaId}/edit")
+	public String initUpdateMecForm(@PathVariable("citaId") final int citaId, final Model model) {
+		Cita cita = this.citaService.findCitaById(citaId);
+		model.addAttribute(cita);
+		return CitaController.VIEWS_MEC_UPDATE_FORM;
+	}
+
+	@PostMapping(value = "/mecanicos/citas/{citaId}/edit")
+	public String processUpdateMecForm(@Valid final Cita citaEditada, @PathVariable("citaId") final int citaId, final BindingResult result, final ModelMap model) {
+		if (citaEditada.getCliente() == null) {
+			citaEditada.setCliente(this.citaService.findCitaById(citaId).getCliente());
+		}
+		if (citaEditada.getMecanico() == null) {
+			citaEditada.setMecanico(this.citaService.findCitaById(citaId).getMecanico());
+		}
+		if (citaEditada.getTipo() == null) {
+			citaEditada.setTipo(this.citaService.findCitaById(citaId).getTipo());
+		}
+		if (citaEditada.getVehiculo() == null) {
+			citaEditada.setVehiculo(this.citaService.findCitaById(citaId).getVehiculo());
+		}
+		if (result.hasErrors()) {
+			model.put("cita", citaEditada);
+			return CitaController.VIEWS_MEC_UPDATE_FORM;
+		} else {
+			Cita citaAntigua = this.citaService.findCitaById(citaId);
+
+			//			BeanUtils.copyProperties(citaAntigua, citaEditada, "id", "fechaCita", "esAceptado", "esUrgente", "tipo", "mecanico", "cliente", "vehiculo"); //
+			BeanUtils.copyProperties(citaEditada, citaAntigua, "id", "esAceptado", "esUrgente", "tipo", "mecanico", "cliente", "vehiculo"); //coge los nuevos descripcion tiempo y coste
+
+			this.citaService.saveCita(citaAntigua);
+
+			return "redirect:/mecanicos/citas/";
+		}
+	}
+
+	@GetMapping("/mecanicos/{vehiculoId}")
+	public String showMecAverListByVeh(final Principal principal, final Map<String, Object> model, @PathVariable("vehiculoId") final int vehiculoId) {
+		Integer mecanicoId = this.mecanicoService.findIdByUsername(principal.getName());
+		Collection<Averia> results = this.mecanicoService.findAveriaByVehiculoId(mecanicoId);
+		model.put("results", results);
+		return "mecanicos/averiasDeVehiculoList";
 	}
 
 }
