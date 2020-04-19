60e436a7b4374083cf0dd901c4226c6a0b094bb5
abemorcardc <abemorcar@alum.us.es>
2020-04-20 01:34:05 +0200

    Corrección en AveriaControllerTests

--- src/test/java/org/springframework/samples/talleres/web/AveriaControllerTests.java
+++ src/test/java/org/springframework/samples/talleres/web/AveriaControllerTests.java
@@ -1,13 +1,6 @@
 
 package org.springframework.samples.talleres.web;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -38,18 +31,18 @@ import org.springframework.samples.talleres.service.CitaService;
 import org.springframework.samples.talleres.service.ClienteService;
 import org.springframework.samples.talleres.service.MecanicoService;
 import org.springframework.samples.talleres.service.VehiculoService;
-import org.springframework.samples.talleres.web.CitaController;
-import org.springframework.samples.talleres.web.VisitController;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 /**
  * Test class for {@link VisitController}
  *
  * @author Colin But
  */
-@WebMvcTest(controllers = CitaController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
+@WebMvcTest(controllers = AveriaController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 class AveriaControllerTests {
 
 	private static final int	TEST_CITA_ID				= 1;
@@ -84,8 +77,8 @@ class AveriaControllerTests {
 	private Mecanico			error;
 
 	private Vehiculo			mercedes;
-	
-	private Usuario				paco1,manolo1;
+
+	private Usuario				paco1, manolo1;
 
 	private Cliente				manolo;
 
@@ -97,17 +90,17 @@ class AveriaControllerTests {
 
 	@BeforeEach
 	void setup() throws ParseException {
-		
-		paco1 = new Usuario();
-		paco1.setNombreUsuario("paco1");
-		paco1.setContra("paco1");
-		paco1.setEnabled(true);
-		
-		manolo1 = new Usuario();
-		manolo1.setNombreUsuario("manolo1");
-		manolo1.setContra("manolo1");
-		manolo1.setEnabled(true);
-		
+
+		this.paco1 = new Usuario();
+		this.paco1.setNombreUsuario("paco1");
+		this.paco1.setContra("paco1");
+		this.paco1.setEnabled(true);
+
+		this.manolo1 = new Usuario();
+		this.manolo1.setNombreUsuario("manolo1");
+		this.manolo1.setContra("manolo1");
+		this.manolo1.setEnabled(true);
+
 		this.paco = new Mecanico();
 		this.paco.setId(AveriaControllerTests.TEST_MECANICO_ID);
 		this.paco.setNombre("Paco");
@@ -119,7 +112,7 @@ class AveriaControllerTests {
 		this.paco.setAveriasArregladas(12);
 		this.paco.setExperiencia("ninguna");
 		this.paco.setTitulaciones("Fp de mecanico");
-		this.paco.setUsuario(paco1);
+		this.paco.setUsuario(this.paco1);
 
 		this.manolo = new Cliente();
 		this.manolo.setId(AveriaControllerTests.TEST_CLIENTE_ID);
@@ -129,18 +122,18 @@ class AveriaControllerTests {
 		this.manolo.setDni("21154416G");
 		this.manolo.setEmail("PacoTalleres@gmail.com");
 		this.manolo.setTelefono("666973647");
-		this.manolo.setUsuario(manolo1);
+		this.manolo.setUsuario(this.manolo1);
 
 		this.mercedes = new Vehiculo();
 		this.mercedes.setId(AveriaControllerTests.TEST_VEHICULO_ID);
 		this.mercedes.setActivo(true);
 		this.mercedes.setKilometraje(10000);
 		this.mercedes.setCliente(this.manolo);
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String string = "2010-09-15";
 		Date fecha = sdf.parse(string);
-		
+
 		Date d = new Date(); // 2012-09-04
 		d.setYear(2012);
 		d.setMonth(9);
@@ -150,17 +143,17 @@ class AveriaControllerTests {
 		this.mercedes.setModelo("Mercedes A");
 		this.mercedes.setTipoVehiculo(TipoVehiculo.turismo);
 
-		this.error = new Mecanico();
-		this.error.setId(2);
-		this.error.setNombre("Error");
-		this.error.setApellidos("Error");
-		this.error.setDireccion("Error");
-		this.error.setDni("Error");
-		this.error.setEmail("Error");
-		this.error.setTelefono("Error");
-		this.error.setAveriasArregladas(0);
-		this.error.setExperiencia("Error");
-		this.error.setTitulaciones("Error");
+		//		this.error = new Mecanico();
+		//		this.error.setId(2);
+		//		this.error.setNombre("Error");
+		//		this.error.setApellidos("Error");
+		//		this.error.setDireccion("Error");
+		//		this.error.setDni("Error");
+		//		this.error.setEmail("Error");
+		//		this.error.setTelefono("Error");
+		//		this.error.setAveriasArregladas(0);
+		//		this.error.setExperiencia("Error");
+		//		this.error.setTitulaciones("Error");
 
 		this.cita1 = new Cita();
 		this.cita1.setId(AveriaControllerTests.TEST_CITA_ID);
@@ -174,7 +167,7 @@ class AveriaControllerTests {
 		this.cita1.setMecanico(this.paco);
 		this.cita1.setVehiculo(this.mercedes);
 		this.cita1.setCliente(this.manolo);
-		
+
 		this.av1 = new Averia();
 		this.av1.setId(1);
 		this.av1.setCita(this.cita1);
@@ -200,9 +193,9 @@ class AveriaControllerTests {
 		this.av2.setNombre("coche de manolo");
 		this.av2.setVehiculo(this.mercedes);
 		this.av2.setMecanico(this.paco);
-		
+
 		BDDMockito.given(this.mecanicoService.findMecIdByUsername("paco1")).willReturn(1);
-		BDDMockito.given(this.averiaService.findAveriaById(1)).willReturn(av1);
+		BDDMockito.given(this.averiaService.findAveriaById(1)).willReturn(this.av1);
 		BDDMockito.given(this.citaService.findCitaById(AveriaControllerTests.TEST_CITA_ID)).willReturn(this.cita1);
 		BDDMockito.given(this.clienteService.findIdByUsername("manolo")).willReturn(AveriaControllerTests.TEST_CLIENTE_ID);
 		BDDMockito.given(this.vehiculoService.findVehiculoById(AveriaControllerTests.TEST_VEHICULO_ID)).willReturn(this.mercedes);
@@ -237,30 +230,29 @@ class AveriaControllerTests {
 		//		this.mockMvc.perform(MockMvcRequestBuilders.get("/mecanicos/1", AveriaControllerTests.TEST_CITA_ID)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("averias/averiasDeVehiculoList"));
 
 	}
-	
+
 	@WithMockUser(value = "paco1", roles = "mecanico")
 	@Test
-	void testInitUpdateForm() throws Exception {	
-		mockMvc.perform(get("/mecanicos/vehiculos/{vehiculoId}/averia/{averiaId}/edit", 1,1)).andExpect(status().isOk())
-				.andExpect(model().attributeExists("averia"))
-				.andExpect(view().name("averias/averiaUpdate"));
+	void testInitUpdateForm() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/mecanicos/vehiculos/{vehiculoId}/averia/{averiaId}/edit", 1, 1)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.model().attributeExists("averia"))
+			.andExpect(MockMvcResultMatchers.view().name("averias/averiaUpdate"));
 	}
 
-//	@WithMockUser(value = "pepe1", roles = "cliente")
-//	@Test
-//	void testProcessUpdateFormSuccess() throws Exception {
-//		mockMvc.perform(post("/cliente/vehiculos/{vehiculoId}/edit", TEST_VEHICULO_ID).with(csrf())
-//				.param("fechaMatriculacion", "2000-12-12").param("tipoVehiculo", "turismo")
-//				.param("matricula", "1234ZXC").param("modelo", "a3234")
-//				.param("kilometraje", "6000").param("activo", "true"))
-//				.andExpect(status().is3xxRedirection())
-//				.andExpect(view().name("redirect:/cliente/vehiculos/"));
-//	}
-//	
-//	@WithMockUser(value = "manolo", roles = "cliente")
-//	@Test
-//	void testInitUpdateFormUsuarioEquivocado() throws Exception {	
-//		mockMvc.perform(get("/cliente/vehiculos/{vehiculoId}/edit", TEST_VEHICULO_ID)).andExpect(status().isOk())
-//				.andExpect(view().name("exception"));
-//	}
+	//	@WithMockUser(value = "pepe1", roles = "cliente")
+	//	@Test
+	//	void testProcessUpdateFormSuccess() throws Exception {
+	//		mockMvc.perform(post("/cliente/vehiculos/{vehiculoId}/edit", TEST_VEHICULO_ID).with(csrf())
+	//				.param("fechaMatriculacion", "2000-12-12").param("tipoVehiculo", "turismo")
+	//				.param("matricula", "1234ZXC").param("modelo", "a3234")
+	//				.param("kilometraje", "6000").param("activo", "true"))
+	//				.andExpect(status().is3xxRedirection())
+	//				.andExpect(view().name("redirect:/cliente/vehiculos/"));
+	//	}
+	//	
+	//	@WithMockUser(value = "manolo", roles = "cliente")
+	//	@Test
+	//	void testInitUpdateFormUsuarioEquivocado() throws Exception {	
+	//		mockMvc.perform(get("/cliente/vehiculos/{vehiculoId}/edit", TEST_VEHICULO_ID)).andExpect(status().isOk())
+	//				.andExpect(view().name("exception"));
+	//	}
 }
