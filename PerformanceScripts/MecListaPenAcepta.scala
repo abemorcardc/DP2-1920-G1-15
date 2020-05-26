e86f79e8706a050ff76b64f7695ea2d69aa8a14f
Flor CL <flocorlop@alum.us.es>
2020-05-26 22:21:19 +0200

    h21 se habia borrado

--- PerformanceScripts/MecListaPenAcepta.scala
+++ PerformanceScripts/MecListaPenAcepta.scala
@@ -5,7 +5,7 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
 
-class MecListaPenAcepta extends Simulation {
+class MecListaPAcepta2 extends Simulation {
 
 	val httpProtocol = http
 		.baseUrl("http://www.dp2.com")
@@ -28,58 +28,80 @@ class MecListaPenAcepta extends Simulation {
 		"Proxy-Connection" -> "keep-alive",
 		"Upgrade-Insecure-Requests" -> "1")
 
-	
-	object GetLogin{
-	val getLogin = exec(http("GetLogin")
+	val headers_3 = Map(
+		"Accept" -> "text/css,*/*;q=0.1",
+		"Proxy-Connection" -> "keep-alive")
+
+
+
+	object GetLogin {
+		val getLogin = exec(http("GetLogin")
 			.get("/login")
 			.headers(headers_0)
 			.resources(http("request_1")
 			.get("/favicon.ico")
 			.headers(headers_1)))
-		.pause(11)
-		}
-
-object PostLogin{
+		.pause(3)
+	}
+	object PostLogin{
 		val postLogin = exec(http("PostLogin")
 			.post("/login")
 			.headers(headers_2)
 			.formParam("username", "paco")
 			.formParam("password", "paco")
-			.formParam("_csrf", "e1a0ba33-9d7f-4005-9482-f8c2aa469df7"))
-		.pause(7)
-		}
-
-object GetCitasP{
+			.formParam("_csrf", "10beccc7-75e5-4094-8f4e-e7d901b91009"))
+			
+		.pause(10)
+		// logged
+	}
+	object GetCitasP {
 		val getCitasP = exec(http("GetCitasP")
 			.get("/mecanicos/citasPendientes")
 			.headers(headers_0))
-		.pause(7)
-}
-object GetAceptar{
-		 val getAceptar = exec(http("GetAceptar")
+			
+		.pause(11)
+		// getcitasp
+	}
+	object GetAcep{
+		val getAcep = exec(http("GetAcep")
 			.get("/mecanicos/citas/4/aceptar")
 			.headers(headers_0))
+
 		.pause(6)
+		// getacep
 		}
-		
-		object PostAceptar{
-		val postAceptar = exec(http("PostAceptar")
+	object Aceptado{
+		val aceptado = exec(http("Aceptado")
 			.post("/mecanicos/citas/4/aceptar")
 			.headers(headers_2)
 			.formParam("id", "4")
-			.formParam("_csrf", "afc3ac33-3140-4a23-b44f-6751bf42087f"))
+			.formParam("_csrf", "c25b409a-f9ed-49d3-a0d2-9b3e2f0edbc5"))
+			
+		.pause(4)
+		// aceptado
+	}
+	object GetAcepN{
+		val getAcepN = exec(http("GetAcepN")
+			.get("/mecanicos/citas/9/aceptar")
+			.headers(headers_0))
+
 		.pause(6)
-}
+		// getacep
+	}
+	object AceptadoN{
+		val aceptadoN = exec(http("AceptadoN")
+			.post("/mecanicos/citas/9/aceptar")
+			.headers(headers_2)
+			.formParam("id", "9")
+			.formParam("_csrf", "c25b409a-f9ed-49d3-a0d2-9b3e2f0edbc5"))
+			
+		.pause(4)
+		// aceptado
+	}
 
-val scn = scenario("MecListaPenAcepta").exec(GetLogin.getLogin, PostLogin.postLogin, GetCitasP.getCitasP, GetAceptar.getAceptar, PostAceptar.postAceptar)
-
-
-	setUp(scn.inject(rampUsers(100) during (30 seconds)))
+val scn = scenario("MecListaPAcepta").exec(GetLogin.getLogin,PostLogin.postLogin, GetCitasP.getCitasP, GetAcep.getAcep, Aceptado.aceptado)
+val scn2 = scenario("MecListaPAceptaNeg").exec(GetLogin.getLogin,PostLogin.postLogin, GetCitasP.getCitasP, GetAcepN.getAcepN, AceptadoN.aceptadoN)
+	setUp(scn.inject(atOnceUsers(1)), 
+		scn2.inject(atOnceUsers(1)))
 	.protocols(httpProtocol)
-	//Codigo de comprobacion de eficacia
-	/*
-	.assertions(global.responseTime.max.lt(5000),
-	global.responseTime.mean.lt(1000),
-	global.successfulRequests.percent.gt(95))
-	*/
 }
