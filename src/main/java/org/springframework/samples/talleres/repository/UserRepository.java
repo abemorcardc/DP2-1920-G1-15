2b7a2d8a570f6f1a39279dfb2ed3c79d3fb7d6d7
Flor CL <flocorlop@alum.us.es>
2020-05-29 10:36:11 +0200

    eliminados repository

--- src/main/java/org/springframework/samples/talleres/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.talleres.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.talleres.model.User;
-
-
-public interface UserRepository extends  CrudRepository<User, String>{
-	
-}
